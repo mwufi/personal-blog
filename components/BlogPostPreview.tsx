b52da5a47e9f36fa329029de3658dd42632d2095
Zen T <ztang230@gmail.com>
2025-05-22 13:41:20 -0500

    d

--- components/BlogPostPreview.tsx
+++ components/BlogPostPreview.tsx
@@ -24,7 +24,7 @@ export function BlogPostPreview({ title, date, description, slug, variant = 'blo
                     <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
                     <Link href={`/blog/${slug}`}>
                         <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                        <span className="relative z-10">{title}</span>
+                        <span className="relative z-10 text-zinc-700">{title}</span>
                     </Link>
                 </h2>
 
